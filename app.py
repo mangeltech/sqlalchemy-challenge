e13552e49e9916e4208cb98039afc7bacd8d4526
mangeltech <61762706+mangeltech@users.noreply.github.com>
2020-05-17 20:26:59 -0500

    hw

---
+++ app.py
@@ -0,0 +1,58 @@
+# import dependencies 
+import datetime as dt
+import numpy as np
+import pandas as pd
+import datetime as dt
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+from flask import Flask, jsonify
+
+
+#################################################
+# Database Setup
+#################################################
+engine = create_engine("sqlite:///Hawaii.sqlite")
+# reflect the database into a new model
+Base = automap_base()
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+# Save reference to the table
+Station = Base.classes.station
+Measurements = Base.classes.measurements
+
+# Create our session (link) from Python to the DB
+session = Session(engine)
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+
+#################################################
+# Flask Routes
+#################################################
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"- List of prior year rain totals from all stations<br/>"
+        f"<br/>"
+        f"/api/v1.0/stations<br/>"
+        f"- List of Station numbers and names<br/>"
+        f"<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"- List of prior year temperatures from all stations<br/>"
+        f"<br/>"
+        f"/api/v1.0/start<br/>"
+        f"- When given the start date (YYYY-MM-DD), calculates the MIN/AVG/MAX temperature for all dates greater than and equal to the start date<br/>"
+        f"<br/>"
+        f"/api/v1.0/start/end<br/>"
+        f"- When given the start and the end date (YYYY-MM-DD), calculate the MIN/AVG/MAX temperature for dates between the start and end date inclusive<br/>"
+
+    )
